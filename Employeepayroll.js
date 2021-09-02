e39befe7436b93eda54c7b59161bb5aa902192fc
kanchan-solanke <kanchanssolanke@gmail.com>
2021-09-03 00:09:29 +0530

    employee payroll class with event listeners

--- Employeepayroll.js
+++ Employeepayroll.js
@@ -1,95 +1,50 @@
-class EmployeePayrollData 
-{
+<!doctype htm>
+    <html>
+    <body>
+        <div id="formId" class="form-content">
+<form class="form" action="#" onreset="resetForm()" onsubmit="save()">
 
-//getter and setter method
+    <div class="form-head">Employee PayRoll Form</div>
+    <div class="row-content">
+        <label class="label text" for="name">Name</label>
+        <input class="input" type="text" id="name" name="name" placeholder="Your Name: "required>
+        <error-output class="text-error" for="text"></error-output>
+    </div>
+</form>
+</div>
 
-get id() 
-{
-    return this._id;
-}
-  set id(id) 
-{
-    this._id = id;
-}
+<script>
 
-get name() 
-{
-      return this._name;
-}
-set name(name)
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) 
+        {
+            textError.textContent = "";
+            return;
+        }
+        try 
+        {
+          (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
 
-{
-      let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+        } catch (e) 
+        {
+            textError.textContent = e;
+        }
        
-      if (nameRegex.test(name)) 
-        this._name = name;
-      else throw "Name is incorrect";
-} 
-get profillePic()
-{
-  return this._profilePic;
-}
-set profillePic(profillePic)
-{
-  this._profilePic = profillePic;
-}
-get geneder()
-{
-  return this._gender;
-}
-set gender(gender)
-{
-  this._gender = gender;
-}
-get department()
-{
-  return this._department;
-}
-set department(department)
-{
-this._department = department;
-}
-get salary()
-{
-  return this._salary;
-}
-set salary(salary)
-{
-  this._salary = salary;
-}
-get note() 
-{
-    return this._note;
-}
-set note(note) 
-{
-    this._note = note;
-}
+    });
 
-get startDate() 
-{
-    return this._startDate;
-}
+       
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+     //in range button the output shown should always be equal to value the user is updating
+        output.textContent = salary.value;
+     salary.addEventListener('input', function () {
+        output.textContent = salary.value;
 
-set startDate(startDate) 
-{
-  let now = new Date();
-  if (startDate > now) throw 'Start Date is a Future Date!'
-     var diff = Math.abs(now.getTime() - startDate.getTime());
-  if (diff / (1000 * 60 * 60 * 24) > 30)
-     throw 'start Date is beyond 30 Days!';
-  this._startDate = startDate;
-}
+     });
+});
 
-
-   //method
-  toString()
-  {
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        //if start date is not in the given datatype it will pass the value undefined to empdate
-        
-        const empDate = !this._startDate ? "undefined" : this._startDate;
-        //here in return for start date we are passing empDate as value                
-        return "id=" + this._id + ", name=" + this._name + " gender= " +this._gender+ " profilePic= "+this.profilePic + " department= "+this._department + " salary= "+ this._salary+ " Note= "+this._note +  " startDate =" + empDate;
-  }
-}
+</script>
